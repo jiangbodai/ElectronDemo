ef56472047d02a6f475e0855469b8a0ee475a1a7
jiangbodai <270223447@qq.com>
2021-05-24 16:59:56 +0800

    添加急诊类型

---
+++ src/store/modules/treatment-tags.js
@@ -0,0 +1,43 @@
+const state = {
+    icons: [{
+      icon: require('@/assets/images/home/icon-triage.png'),
+      title: '分诊系统',
+      name: 'triage',
+    },{
+      icon: require('@/assets/images/home/icon-moveInfusion.png'),
+      title: '移动输液',
+      name: 'infusion'
+    },{
+      icon: require('@/assets/images/home/icon-patient.png'),
+      title: '患者管理',
+      name: 'patient'
+    },{
+      icon: require('@/assets/images/home/icon-qualityControl.png'),
+      title: '质控管理',
+      name: 'qualityControl'
+    },{
+      icon: require('@/assets/images/home/icon-shifts.png'),
+      title: '交接班',
+      name: 'shifts'
+    },{
+      icon: require('@/assets/images/home/icon-firstAid.png'),
+      title: '院前急救',
+      name: 'firstAid'
+    },{
+      icon: require('@/assets/images/home/icon-critically.png'),
+      title: '危急重症中心',
+      name: 'critically'
+    },{
+      icon: require('@/assets/images/home/icon-departments.png'),
+      title: '科室管理',
+      name: 'departments'
+    },{
+      icon: require('@/assets/images/home/icon-setting.png'),
+      title: '系统设置',
+      name: 'setting'
+    }]
+  }
+
+  export default {
+    state
+  }
